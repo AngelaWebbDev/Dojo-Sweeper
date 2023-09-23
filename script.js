46e9b60c8bb26a892b63c6b68185d264c4481cb5
Angela Webb <angelawebb.dev@gmail.com>
2023-09-22 21:28:14 -0400

    replaces theDojo[] with tenNinjas[] everywhere

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-var theDojo = [ [1, 0, 1, 1, 1, 0, 4, 0, 8, 0],
-                [3, 1, 0, 7, 0, 0, 6, 0, 8, 8],
-                [5, 0, 7, 0, 3, 6, 6, 6, 0, 0],
-                [2, 3, 0, 9, 0, 0, 6, 0, 8, 0],
-                [6, 0, 3, 3, 0, 2, 0, 3, 0, 4],
-                [0, 0, 3, 3, 0, 0, 2, 2, 3, 0],
-                [0, 0, 0, 0, 5, 0, 1, 2, 0, 6],
-                [2, 2, 2, 2, 0, 7, 1, 1, 1, 0],
-                [5, 2, 0, 2, 0, 0, 0, 1, 1, 2],
-                [9, 2, 2, 2, 0, 7, 0, 1, 1, 0] ];
+var tenNinjas = [ 
+    [0, 0 ,0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0 ,0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0 ,0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0 ,0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0 ,0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0 ,0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0 ,0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0 ,0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0 ,0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0 ,0, 0, 0, 0, 0, 0, 0, 0] 
+    ];
 var dojoDiv = document.querySelector("#the-dojo");
     
 // Creates the rows of buttons for this game
-function render(theDojo) {
+function render(tenNinjas) {
     var result = "";
-    for(var rowNum=0; rowNum<theDojo.length; rowNum++) {
-        for(var colNum=0; colNum<theDojo[rowNum].length; colNum++) {
+    for(var rowNum=0; rowNum<tenNinjas.length; rowNum++) {
+        for(var colNum=0; colNum<tenNinjas[rowNum].length; colNum++) {
             //add id number for writing ninjas on square when clicked
             result += `<button class="tatami" id="${rowNum}${colNum}" onclick="howMany(${rowNum}, ${colNum}, this)"></button>`;
         }
@@ -25,19 +27,6 @@ function render(theDojo) {
 
 // BONUS CHALLENGE 2: at the start randomly place 10 ninjas into theDojo with at most 1 on each spot
 function hideNinjas(){
-    //create a copy of theDojo with all numbers being 0
-    var tenNinjas = [ 
-                    [0, 0 ,0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0 ,0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0 ,0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0 ,0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0 ,0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0 ,0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0 ,0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0 ,0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0 ,0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0 ,0, 0, 0, 0, 0, 0, 0, 0] 
-                    ];
     //for count of 10 (to hide 10 ninjas)
     for(var i=0;i<10;i++){
         //math.random row and col numbers (0-9)
@@ -69,15 +58,15 @@ function howMany(rowNum, colNum, element) {
     
     var ninjas = 0;
     for(var row = rowNum-1 ; row<=rowNum+1 ; row++){ /* check rows --,++,== */
-        if(row>=0 && row<=theDojo.length){ /* is row a valid row number? */
+        if(row>=0 && row<=tenNinjas.length){ /* is row a valid row number? */
             for(var col = colNum-1 ; col<=colNum+1 ; col++){ /* check cols --,++,== */
-                if(col>=0 && col<=theDojo[row].length){ /* is col a valid col number? */
-                    ninjas+=theDojo[row][col]; /* add to ninja count */
+                if(col>=0 && col<=tenNinjas[row].length){ /* is col a valid col number? */
+                    ninjas+=tenNinjas[row][col]; /* add to ninja count */
                 }
             }
         }
     }
-    ninjas-=theDojo[rowNum][colNum]; /* do not include ninjas in clicked square */
+    ninjas-=tenNinjas[rowNum][colNum]; /* do not include ninjas in clicked square */
 
     // BONUS CHALLENGE 1: draw the number onto the button instead of alerting it
     playboard.innerHTML = ninjas;
@@ -89,7 +78,7 @@ function howMany(rowNum, colNum, element) {
 var style="color:cyan;font-size:1.5rem;font-weight:bold;";
 
 // adds the rows of buttons into <div id="the-dojo"></div> 
-dojoDiv.innerHTML = render(theDojo);    
+dojoDiv.innerHTML = render(tenNinjas);    
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //                                               BONUS CHALLENGES                                           //
